793695a0f5499260ad8ac3c3cdb0d219e81e10af
korl-z <korl.zeleznikar@gmail.com>
2025-09-04 20:48:10 +0200

    setup file (ml import problems)

--- ml/common/data_utils/data_modules.py
+++ ml/common/data_utils/data_modules.py
@@ -1,7 +1,3 @@
-import sys
-
-sys.path.insert(1, "C:/Users/Uporabnik/Documents/IJS-F9/korlz")
-
 import logging
 import multiprocessing
 from typing import Type
--- ml/common/data_utils/gauss_rank_scaler.py
+++ ml/common/data_utils/gauss_rank_scaler.py
@@ -177,9 +177,6 @@ if __name__ == "__main__":
     # test
 
     import matplotlib.pyplot as plt
-    # import sys 
-
-    # sys.path.insert(1, "C:/Users/Uporabnik/Documents/IJS-F9/korlz")
 
     from ml.custom.higgs.analysis.utils import run_chainer
 
